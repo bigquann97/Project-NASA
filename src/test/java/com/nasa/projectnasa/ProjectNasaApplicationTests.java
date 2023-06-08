2689ffffea38ade5c976fddc02b5468d0509f53d
Gwanho KIM <bigquan977@gmail.com>
2023-06-08 18:40:05 +0900

    [FEAT]: Project Main

---
+++ src/test/java/com/nasa/projectnasa/ProjectNasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.projectnasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNasaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
