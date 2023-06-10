73092b535a383ec61efbcb17187d35e30b118985
Gwanho KIM <bigquan977@gmail.com>
2023-06-10 23:59:41 +0900

    [FEAT]: BaseEntity 생성

---
+++ src/main/java/com/nasa/projectnasa/common/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.nasa.projectnasa.common.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import net.bytebuddy.asm.Advice.Local;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+public class BaseEntity {
+
+  @CreatedBy
+  private Long createdAccountId;
+
+  @LastModifiedBy
+  private Long modifiedAccountId;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime modifiedAt;
+
+}
