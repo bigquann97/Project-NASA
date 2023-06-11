5d7fcf5f100d776163c05ec82dc50a88903687cd
Gwanho KIM <bigquan977@gmail.com>
2023-06-11 23:59:08 +0900

    [FEAT]: Wish Entity 기본

--- src/main/java/com/nasa/projectnasa/wish/entity/Wish.java
+++ src/main/java/com/nasa/projectnasa/wish/entity/Wish.java
@@ -1,5 +1,15 @@
 package com.nasa.projectnasa.wish.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class Wish {
 
+  @Id
+  @GeneratedValue
+  private Long id;
 }
