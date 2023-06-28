225a0605fb98f0bff29afb9979d50b580cf9f6c1
Gwanho KIM <bigquan977@gmail.com>
2023-06-28 21:43:52 +0900

    [FEAT]: EnableJpaAuditing

--- src/main/java/com/nasa/projectnasa/ProjectNasaApplication.java
+++ src/main/java/com/nasa/projectnasa/ProjectNasaApplication.java
@@ -2,12 +2,22 @@ package com.nasa.projectnasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectNasaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectNasaApplication.class, args);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
